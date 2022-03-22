a88f63eefb92db1b92f43c08dba8b31c1cf6bd3f
Avanaganti Saichand <sai.avanaganti@realvariable.com>
2022-03-22 11:40:15 +0530

    added names

--- src/App.js
+++ src/App.js
@@ -6,36 +6,25 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Saichand",
-      age: "21",
+      monsters: [
+        {
+          name: "Linda",
+        },
+        {
+          name: "Banda",
+        },
+        {
+          name: "Jacky",
+        },
+      ],
     };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.name}</p>
-          <p>{this.state.age}</p>
-          <button
-            onClick={() => {
-              this.setState({
-                name: "Avanganti",
-                age: "42",
-              });
-            }}
-          >
-            Change
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
